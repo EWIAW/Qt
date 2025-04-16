8c5d4c4bfef280333c2b7c81a335b62b7157514e
EWIAW <2450925561@qq.com>
2025-04-16 22:21:37 +0800

    学习了一个Qt的控件，以及控件的一些设置

---
+++ QWidget/widget.cpp
@@ -0,0 +1,107 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QPushButton>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+//    QPushButton* btn = new QPushButton("按钮",this);
+//    btn->setEnabled(false);
+//    this->setWindowTitle("标题");
+//    QIcon icon(":/photo.jpg");//创建窗口图标对象，并且表面在qrc文件中查找
+//    this->setWindowIcon(icon);//设置图标
+//    QPushButton* btn = new QPushButton(this);
+//    btn->setText("按钮");
+//    btn->resize(200,200);
+//    btn->setCursor(QCursor(Qt::WaitCursor));
+    QPixmap pixmap(":/photo.jpg");
+    pixmap = pixmap.scaled(64,64);
+    QCursor cursor(pixmap,2,2);
+    ui->pushButton->setCursor(cursor);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+//void Widget::on_pushButton_clicked()
+//{
+//    qDebug()<<"按钮被按下";
+//}
+
+//void Widget::on_pushButton_2_clicked()
+//{
+//    bool flag = ui->pushButton->isEnabled();
+//    ui->pushButton->setEnabled(!flag);
+//}
+
+//void Widget::on_pushButton_up_clicked()
+//{
+//    QRect rect = ui->pushButton_target->geometry();
+//    ui->pushButton_target->setGeometry(rect.x(),rect.y()-5,rect.width(),rect.height());
+//}
+
+//void Widget::on_pushButton_down_clicked()
+//{
+//    QRect rect = ui->pushButton_target->geometry();
+//    ui->pushButton_target->setGeometry(rect.x(),rect.y()+5,rect.width(),rect.height());
+//}
+
+//void Widget::on_pushButton_right_clicked()
+//{
+//    QRect rect = ui->pushButton_target->geometry();
+//    ui->pushButton_target->setGeometry(rect.x()+5,rect.y(),rect.width(),rect.height());
+//}
+
+//void Widget::on_pushButton_left_clicked()
+//{
+//    QRect rect = ui->pushButton_target->geometry();
+//    ui->pushButton_target->setGeometry(rect.x()-5,rect.y(),rect.width(),rect.height());
+//}
+
+//void Widget::on_pushButton_accept_clicked()
+//{
+//    ui->label->setText("hhh");
+//}
+
+//void Widget::on_pushButton_reject_pressed()
+//{
+//    int width = this->geometry().width();
+//    int height = this->geometry().height();
+//    qDebug()<<width<<" "<<height;
+
+//    int x = rand()%width;
+//    int y = rand()%height;
+//    qDebug()<<x<<" "<<y;
+//    ui->pushButton_reject->move(x,y);
+//}
+
+void Widget::on_pushButton_add_clicked()
+{
+    float opacity = this->windowOpacity();
+    if(opacity >= 1.0)
+    {
+        return;
+    }
+
+    opacity += 0.1;
+    this->setWindowOpacity(opacity);
+}
+
+void Widget::on_pushButton_sub_clicked()
+{
+    float opacity = this->windowOpacity();
+    if(opacity <= 0.5)
+    {
+        return;
+    }
+
+    opacity -= 0.1;
+    this->setWindowOpacity(opacity);
+}
---
+++ QWidget/widget.h
@@ -0,0 +1,42 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+//    void on_pushButton_clicked();
+
+//    void on_pushButton_2_clicked();
+
+//    void on_pushButton_up_clicked();
+
+//    void on_pushButton_right_clicked();
+
+//    void on_pushButton_left_clicked();
+
+//    void on_pushButton_down_clicked();
+
+//    void on_pushButton_accept_clicked();
+
+//    void on_pushButton_reject_pressed();
+
+    void on_pushButton_add_clicked();
+
+    void on_pushButton_sub_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
